f7a0105096a959fd45fd461349d48972c58101dc
Unmesh28 <unmesh.dabhade@gmail.com>
2022-05-02 09:02:41 +0530

    more test cases added

--- test/inbox_test.js
+++ test/inbox_test.js
@@ -8,18 +8,30 @@ const web3 = new Web3(ganache.provider());
 let accounts;
 let inbox;
 
+
 beforeEach( async () => {
     //get a list of all accounts
     accounts = await web3.eth.getAccounts();
     //use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({data : bytecode, arguments: ['Hi there']})
-        .send({from : accounts[0], gas: '1000000'})
+        .deploy({data : bytecode, arguments: ['Hi There']})
+        .send({from : accounts[0], gas: '1000000'});
 
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async() => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi There');
+    });
+
+    it('can modify message', async() => {
+        await inbox.methods.setMessage('Bye').send({from : accounts[0], gas : '1000000'});
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Bye');
     });
 });
